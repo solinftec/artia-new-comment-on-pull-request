6b6227492da85f1954043ed7eabe6f7c72484f48
Victor Gonçalves Vargas <91100014+Notorious19@users.noreply.github.com>
2021-11-30 11:07:21 -0300

    removing backslashes from comment

--- dist/index.js
+++ dist/index.js
@@ -289,7 +289,7 @@ const ArtiaComment = pull_request.body
     .pop()
     .split('End Artia Comment')[0]
     //Remove "\" do comentário do Artia
-    .replace(/\\/g, '\\');
+    .replace(/\\/g, '/');
 console.log('\n OrganizationId =>', organizationId, '\n');
 console.log('\n ActivityId =>', activityId, '\n');
 console.log('\n ArtiaUrl =>', artiaUrl, '\n');
