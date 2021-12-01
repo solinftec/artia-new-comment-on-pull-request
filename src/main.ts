7df8f6102ce6302339aae504291c0e3056c2fc81
Victor Gonçalves Vargas <91100014+Notorious19@users.noreply.github.com>
2021-12-01 16:01:42 -0300

    sem aspas

--- src/main.ts
+++ src/main.ts
@@ -33,6 +33,7 @@ const ArtiaComment = pull_request.body
   .split('End Artia Comment')[0]
   //Remove "\" do comentário do Artia
   .replace(/\\/g, '/')
+  .replace(/['"]+/g, "'")
 
 console.log('\n OrganizationId =>', organizationId, '\n')
 console.log('\n ActivityId =>', activityId, '\n')
